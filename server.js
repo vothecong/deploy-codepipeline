cce721329f06990fc92ed08ac8380f61139e8e9e
vothecong <62562823+vothecong@users.noreply.github.com>
2022-12-25 21:37:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 3000;
-const version = 2;
+const version = 1;
 
 app.get("/", (req, res) => {
     res.send(`hello from simple server :)😂, version: ${version}`);
