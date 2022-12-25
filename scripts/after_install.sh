f96e93a743393d1aac5c55cdf0b18d2fa5e3e9dc
vtconga4tqtpy@gmail.com <vtconga4tqtpy@gmail.com>
2022-12-25 22:31:00 +0700

    update v.0.1

--- scripts/after_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/deploy-codepipeline/deploy.log
-
-echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/deploy-codepipeline/deploy.log
-cd /home/ec2-user/deploy-codepipeline >> /home/ec2-user/deploy-codepipeline/deploy.log
-
-echo 'npm install' >> /home/ec2-user/deploy-codepipeline/deploy.log 
-npm install >> /home/ec2-user/deploy-codepipeline/deploy.log
