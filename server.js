c1f9f425d4ae08e026647db70963865f4dcd8fc0
vtconga4tqtpy@gmail.com <vtconga4tqtpy@gmail.com>
2022-12-25 22:50:18 +0700

    updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 3000;
-const version = 1;
+const version = 23;
 
 app.get("/", (req, res) => {
     res.send(`hello from simple server :)😂, version: ${version}`);
